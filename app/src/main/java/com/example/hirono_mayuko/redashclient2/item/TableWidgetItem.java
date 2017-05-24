969dc430fff682e651a374ae7d3ce8cf440fcb90
Bambina <mayuko.hirono@gmail.com>
2017-05-24 22:53:24 +0900

    WIP

--- app/src/main/java/com/example/hirono_mayuko/redashclient2/item/TableWidgetItem.java
+++ app/src/main/java/com/example/hirono_mayuko/redashclient2/item/TableWidgetItem.java
@@ -1,11 +1,28 @@
 package com.example.hirono_mayuko.redashclient2.item;
 
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.text.Html;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
 import com.example.hirono_mayuko.redashclient2.R;
+import com.example.hirono_mayuko.redashclient2.TableLastPage;
 import com.example.hirono_mayuko.redashclient2.activity.MainActivity;
 import com.example.hirono_mayuko.redashclient2.widget.TableWidget;
 import com.example.hirono_mayuko.redashclient2.databinding.ItemTableBinding;
 import com.xwray.groupie.Item;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -17,7 +34,7 @@ public class TableWidgetItem extends Item<ItemTableBinding> {
     private TableWidget mWidget;
     private MainActivity mainActivity;
 
-    public TableWidgetItem(String widgetId, HashMap<String, String> visualData, MainActivity activity){
+    public TableWidgetItem(String widgetId, HashMap<String, String> visualData, MainActivity activity) {
         super();
         mWidgetId = widgetId;
         mainActivity = activity;
@@ -25,18 +42,130 @@ public class TableWidgetItem extends Item<ItemTableBinding> {
     }
 
     @Override
-    public void bind(ItemTableBinding binding, int position){
+    public void bind(ItemTableBinding binding, int position) {
         binding.setTableWidget(mWidget);
-        if(mWidget.mVisualName == null){
+        if (mWidget.mVisualName == null) {
             return;
         }
+
+        binding.pager.setAdapter(new TablePagerAdapter());
+        binding.tabLayout.setupWithViewPager(binding.pager, true);
+
+        System.out.println(mWidget.getData().get(0).get(0));
     }
 
-    @Override public int getLayout() {
+    @Override
+    public int getLayout() {
         return R.layout.item_table;
     }
 
-    public void notifyWidgetChanged(){
+    public void notifyWidgetChanged() {
         mainActivity.notifyItemChanged(this);
+    }
+
+    private class TablePagerAdapter extends PagerAdapter {
+        private int numPages;
+        private Context mContext;
+
+        public TablePagerAdapter() {
+            super();
+            mContext = mainActivity.getContext();
+            int size = mWidget.getData().size();
+            if (size == 5) {
+                numPages = 6;
+            } else {
+                numPages = size;
+            }
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            System.out.println("posi: " + position);
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (position == 5) {
+                View v = inflater.inflate(R.layout.table_last_page, container, false);
+                container.addView(v);
+                return v;
+            } else {
+                View v = inflater.inflate(R.layout.fragment_table_page, container, false);
+                container.addView(v);
+                setTableData(v, position);
+                return v;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return numPages;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((LinearLayout) object);
+        }
+
+        private void setTableData(View v, int position) {
+            TableLayout mTableView = (TableLayout) v.findViewById(R.id.gridView);
+            ArrayList<ArrayList<String>> mPageData = mWidget.getPageData(position);
+
+            int numColumn = mPageData.get(0).size();
+            float columnWeight = 1f / numColumn;
+
+            for (int p = 0; p < mPageData.size(); p++) {
+                ArrayList<String> row = mPageData.get(p);
+                TableRow tableRow = new TableRow(mContext);
+                TableRow.LayoutParams params = new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.WRAP_CONTENT);
+                tableRow.setLayoutParams(params);
+                tableRow.setBackgroundColor(0xfff0f0f0);
+
+                // create a new linear layout to be added
+                params = new TableRow.LayoutParams(0,
+                        TableRow.LayoutParams.WRAP_CONTENT);
+                LinearLayout layout = new LinearLayout(mContext);
+                params.weight = 1;
+                layout.setLayoutParams(params);
+                layout.setWeightSum(1);
+
+                for (int i = 0; i < row.size(); i++) {
+                    String column = row.get(i);
+                    TextView textView = new TextView(mContext);
+                    if (p == 0) {
+                        textView.setBackgroundColor(0xfff0f0f0);
+                    } else {
+                        textView.setBackgroundColor(0xffffffff);
+                    }
+                    textView.setPadding(30, 30, 30, 30);
+                    LinearLayout.LayoutParams childParams = new LinearLayout.LayoutParams(0,
+                            LinearLayout.LayoutParams.WRAP_CONTENT);
+                    childParams.weight = columnWeight;
+                    childParams.setMargins(0, 0, 0, 1);
+                    textView.setLayoutParams(childParams);
+                    if (i == 0) {
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                            textView.setText(Html.fromHtml(column, Html.FROM_HTML_MODE_LEGACY));
+                        } else {
+                            textView.setText(Html.fromHtml(column));
+                        }
+                        textView.setLinkTextColor(ContextCompat.getColor(mContext, R.color.colorAccent));
+                    } else {
+                        textView.setText(column);
+                    }
+                    textView.setMaxLines(1);
+                    textView.setSingleLine(true);
+                    textView.setEllipsize(TextUtils.TruncateAt.END);
+                    layout.addView(textView);
+                }
+                tableRow.addView(layout);
+                mTableView.addView(tableRow);
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/hirono_mayuko/redashclient2/widget/TableWidget.java
+++ app/src/main/java/com/example/hirono_mayuko/redashclient2/widget/TableWidget.java
@@ -6,8 +6,13 @@ import com.example.hirono_mayuko.redashclient2.item.TableWidgetItem;
 import com.example.hirono_mayuko.redashclient2.model.Widget;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by hirono-mayuko on 2017/04/24.
@@ -18,6 +23,10 @@ public class TableWidget extends Widget {
     private MainActivity mainActivity;
     private TableWidgetItem mItem;
 
+    private ArrayList<String> mColNames;
+    private static final int NUM_ROWS = 6;
+    private static final int NUM_PAGES = 5;
+    private ArrayList<ArrayList<ArrayList<String>>> mData;
     public String mVisualName;
     public String mQueryName;
 
@@ -29,6 +38,39 @@ public class TableWidget extends Widget {
     }
 
     public void setData(JSONArray dataArray) {
+        // Get column names.
+        try {
+            mColNames = getColNames(dataArray.getJSONObject(0));
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+        mData = new ArrayList<>(NUM_PAGES);
+        int len = dataArray.length();
+        if(len > 25) len = 25;
+
+        ArrayList<ArrayList<String>> page = new ArrayList<>(NUM_ROWS);
+        page.add(mColNames);
+        for(int i=0; i < len; i++){
+            if(i != 0 && i%NUM_PAGES == 0){
+                mData.add(page);
+                page = new ArrayList<>(NUM_ROWS);
+                page.add(mColNames);
+            }
+
+            ArrayList<String> row = new ArrayList<>();
+            try {
+                JSONObject obj = dataArray.getJSONObject(i);
+                for(String col:mColNames){
+                    String val = obj.getString(col);
+                    row.add(val);
+                }
+                page.add(row);
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        mData.add(page);
+
         mVisualName = mVisualData.get(Dashboard.VISUAL_NAME);
         mQueryName = mVisualData.get(Dashboard.QUERY_NAME);
         mItem.notifyWidgetChanged();
@@ -36,5 +78,24 @@ public class TableWidget extends Widget {
 
     public void callback(JSONArray dataArray){
         this.setData(dataArray);
+    }
+
+    private ArrayList<String> getColNames(JSONObject obj){
+        Iterator<String> keys = obj.keys();
+        ArrayList<String> colNames = new ArrayList<>();
+        while(keys.hasNext()){
+            String name = keys.next();
+            colNames.add(name);
+            System.out.println(name);
+        }
+        return colNames;
+    }
+
+    public ArrayList<ArrayList<String>> getPageData(int position){
+        return mData.get(position);
+    }
+
+    public ArrayList<ArrayList<ArrayList<String>>> getData(){
+        return mData;
     }
 }
